33319e21a40f38e250ca6076a8192cdfccbe1507
mwinant <mwinant@nic.edu>
2024-09-12 10:34:19 -0700

    added Folders -Mikayla

---
+++ Assets/Mikayla_Assets/Mikayla_Scripts/EnemyAttributes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class enemyAttributes : MonoBehaviour
+{
+    [SerializeField] protected int health;
+    [SerializeField] protected int damage;
+    [SerializeField] protected int armor;
+    [SerializeField] protected int speed;
+    void Start(){
+        //GameObject spawner = GameObject.Find("enemySpawner");
+    }
+    public void enemyInit(int hp, int dmg, int arm, int spd, int diff)
+    {
+        health = hp * diff;
+        damage = dmg * diff;
+        armor = arm * diff;
+        speed = spd;
+    }
+        
+}

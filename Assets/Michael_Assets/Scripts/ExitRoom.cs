7816aa82ba924c48dda332b45ee59a967bdedd78
MJHabermann <MJHabermann@students.nic.edu>
2024-10-08 14:58:32 -0700

    Added a test case

---
+++ Assets/Michael_Assets/Scripts/ExitRoom.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitRoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public BoxCollider2D ExitTrigger;
+    // Update is called once per frame
+    void Update() { 
+    }
+}

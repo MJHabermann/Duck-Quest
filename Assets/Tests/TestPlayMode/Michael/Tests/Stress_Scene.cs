dd82769c81e1c5796f99e116667f189dae2f0de3
MJHabermann <MJHabermann@students.nic.edu>
2024-10-16 19:34:10 -0700

    Made tests better

--- Assets/Tests/TestPlayMode/Michael/Tests/Stress_Scene.cs
+++ Assets/Tests/TestPlayMode/Michael/Tests/Stress_Scene.cs
@@ -8,14 +8,15 @@ public class Stress_Exits: MonoBehaviour
     [UnityTest]
     public IEnumerator RapidSceneTransition()
     {
-        int transitions = 50;
+        float transitions = 10.0f;
         for (int i = 0; i < transitions; i++)
         {
             SceneManager.LoadScene("PlayerRoom");
-            yield return new WaitForSeconds((float)(1- transitions/50));
+            yield return new WaitForSeconds((float)(1.0 - i/transitions));
             SceneManager.LoadScene("Town");
-            yield return new WaitForSeconds((float)(1 - transitions / 50));
-            Debug.Log("Loading Iteration:" + i);
+            yield return new WaitForSeconds((float)(1.0 - i/transitions));
+
+            Debug.Log("Loading Iteration:" + i + " Waited " + ((float)(1.0 - i/ transitions)) + " inbetween Transitions");
         }
     }
 }

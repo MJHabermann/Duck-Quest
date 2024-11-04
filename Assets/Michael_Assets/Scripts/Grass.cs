3b2d0ae709d3cce2b1c7461a40cf8a7c9de68dd5
MJHabermann <MJHabermann@students.nic.edu>
2024-11-04 13:46:10 -0800

    Added a better grass script with dynamic binding

--- Assets/Michael_Assets/Scripts/Grass.cs
+++ Assets/Michael_Assets/Scripts/Grass.cs
@@ -2,15 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grass : MonoBehaviour
+public class DestroyableObject: MonoBehaviour
 {
     // Start is called before the first frame update
+  public virtual void DestroyObject()
+    {
+        Destroy(gameObject);
+        Debug.Log("Destroyed Some Object");
+    }
+}
+
+
+public class Grass : DestroyableObject
+{
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Sword"))
+        if (other.CompareTag("Sword"))
         {
-            Destroy(gameObject);
-            Debug.Log("destroyed Grass Block");
+            DestroyObject();
         }
     }
+    public GameObject drops;
+    public override void DestroyObject()
+    {
+        
+        Debug.Log("Destroyed Grass Block");
+        Vector3 grassPosition = transform.position;
+        Debug.Log(grassPosition.x + " " + grassPosition.y + " " + grassPosition.z);
+        if (drops != null) 
+        {
+            Instantiate(drops, grassPosition, Quaternion.identity);
+            Debug.Log("Item Dropped");
+        }
+        else
+        {
+            Debug.LogWarning("Item is not assigned to object");
+        }
+        Destroy(gameObject);
+    }
+
 }

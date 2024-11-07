9bcaf3861c6f8ccd7343ec8af34f3b0a849695d6
ryansten20 <ryansten20@outlook.com>
2024-11-06 16:47:33 -0800

    s

--- Assets/Ryan_Assets/Script/JoystickMove.cs
+++ Assets/Ryan_Assets/Script/JoystickMove.cs
@@ -1,13 +1,15 @@
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class JoystickMove : MonoBehaviour
 {
-    public GameObject joystickGameObject; // Reference to the joystick GameObject
-#if UNITY_ANDROID
-    public Joystick movementJoystick; // Joystick reference, only for Android
-#endif
 
+    public GameObject joystick;
+    public Joystick movementJoystick;
     public float moveSpeed = 4f;
     private Rigidbody2D rb;
 
@@ -21,79 +23,75 @@ public class JoystickMove : MonoBehaviour
     private Animator animator;
     private Vector3 playerDirection;
 
+    //public AudioSource playerStep;
+
+    // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         reachCollider = reach.GetComponent<Collider2D>();
         playerDirection = new Vector3(0, 0, 0);
-
-#if UNITY_ANDROID
-        // Activate joystick only on Android
-        if (joystickGameObject != null)
-        {
-            joystickGameObject.SetActive(true);
-        }
-#else
-        // Deactivate joystick on non-Android platforms
-        if (joystickGameObject != null)
-        {
-            joystickGameObject.SetActive(false);
-        }
-#endif
+        joystick.SetActive(true);
     }
-
+    // Update is called once per frame
     private void FixedUpdate()
     {
-#if UNITY_ANDROID
-        // Get input direction from joystick on Android
-        moveInput = movementJoystick.Direction;
-#endif
+    // Get input direction directly from the joystick
+    moveInput = movementJoystick.Direction;
 
-        // If there is movement input...
-        if (moveInput != Vector2.zero)
-        {
-            bool success = MovePlayer(moveInput);
-
-            // If collision occurs, try alternate directions
-            if (!success)
-            {
-                success = MovePlayer(new Vector2(moveInput.x, 0)) || MovePlayer(new Vector2(0, moveInput.y));
-            }
-
-            animator.SetBool("isMoving", success);
-            animator.SetFloat("XInput", moveInput.x);
-            animator.SetFloat("YInput", moveInput.y);
-
-            // Adjust player direction based on input for animation purposes
-            if (moveInput.x > 0)
-                playerDirection = new Vector3(0, 0, 0); // Right
-            else if (moveInput.x < 0)
-                playerDirection = new Vector3(0, 0, 180); // Left
-            if (moveInput.y > 0)
-                playerDirection = new Vector3(0, 0, 90); // Up
-            else if (moveInput.y < 0)
-                playerDirection = new Vector3(0, 0, 270); // Down
-        }
-        else
-        {
-            rb.velocity = Vector2.zero;
-            animator.SetBool("isMoving", false);
-        }
-    }
-
-    public bool MovePlayer(Vector2 direction)
+    // If there is movement input...
+    if (moveInput != Vector2.zero)
     {
-        int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+        // Try to move player in input direction
+        bool success = MovePlayer(moveInput);
 
-        if (count == 0)
+        // If collision occurs, try alternate directions
+        if (!success)
         {
+            success = MovePlayer(new Vector2(moveInput.x, 0)) || MovePlayer(new Vector2(0, moveInput.y));
+        }
+
+        // Set isMoving based on movement success
+        animator.SetBool("isMoving", success);
+
+        // Update animator direction parameters
+        animator.SetFloat("XInput", moveInput.x);
+        animator.SetFloat("YInput", moveInput.y);
+
+        // Adjust player direction based on input for animation purposes
+        if (moveInput.x > 0)
+            playerDirection = new Vector3(0, 0, 0); // Right
+        else if (moveInput.x < 0)
+            playerDirection = new Vector3(0, 0, 180); // Left
+        if (moveInput.y > 0)
+            playerDirection = new Vector3(0, 0, 90); // Up
+        else if (moveInput.y < 0)
+            playerDirection = new Vector3(0, 0, 270); // Down
+
+        // Optionally, broadcast player direction changes if other scripts need it
+        //gameObject.BroadcastMessage("PlayerDirection", playerDirection, SendMessageOptions.DontRequireReceiver);
+    }
+    else
+    {
+        // No input detected, stop movement
+        rb.velocity = Vector2.zero;
+        animator.SetBool("isMoving", false);
+    }
+}
+
+    public bool MovePlayer(Vector2 direction){
+        //Check if there are any collisions in intended directions
+        int count = rb.Cast(direction,
+                            movementFilter,
+                            castCollisions,
+                            moveSpeed * Time.fixedDeltaTime + collisionOffset);
+        //if none, update position and return true
+        if(count == 0){
             Vector2 moveVector = direction * moveSpeed * Time.fixedDeltaTime;
             rb.MovePosition(rb.position + moveVector);
             return true;
-        }
-        else
-        {
+        }else {
             return false;
         }
     }
